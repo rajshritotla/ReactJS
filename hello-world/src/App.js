e3830e66fbaec86987a1ff75e5fed9f1924b801a
Rajshri Totla <rajshreetotla@gmail.com>
2019-08-29 02:16:24 -0400

    tried 2 methods in comments now

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,25 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
 }
+
+
+// WAY 1
+/*
+    function App() {
+      return (
+        showmessage
+      );
+  }
+*/
+// WAY 2
+/*
+    class App extends React.Component {
+      render() {
+        return (showmessage);
+      }
+}*/
+
+/*
+    const showmessage = (
+      <div>
+        <h1>Hello, World!</h1><p>I am learning react</p>
+      </div>
+    );
+*/
+
 
 export default App;
